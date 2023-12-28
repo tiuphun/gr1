f03e1bb85544578a772c277e9de0e2fc1e25930f
tiuphun <phuong.nt210692@sis.hust.edu.vn>
2023-12-28 16:19:21 +0700

    done GR1

---
+++ sub.py
@@ -0,0 +1,23 @@
+import paho.mqtt.client as mqtt
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("esp/dht/temperature")
+    client.subscribe("esp/dht/humidity")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("broker.hivemq.com", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
